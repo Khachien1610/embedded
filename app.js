a15e4cd662025068798cb0910a32e8a5d5ab6949
Khắc Hiển <khachien1610@gmail.com>
2022-06-22 23:28:35 +0700

    initial project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/api/test", (req, res) => {
+  res.status(200).json({
+    message: "Duy oc cho",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on ${PORT}`);
+});
